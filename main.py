69a494bf964dc560a31776080c6299edae48ac49
retro <retro@retros-mbp.home>
2022-06-11 17:30:43 +0100

    fix video link edge case and support multiple variables

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ import pandas as pd
 import argparse
 import requests_cache
 from tenacity import *
-import os, errno
+import os
+import errno
 import hashlib
 
-parser = argparse.ArgumentParser("Dump speedrun.com leaderboard to JSON/CSV/SQLite")
-parser.add_argument('game', metavar='G', nargs=None, help='id of game') 
-parser.add_argument('category', metavar='C', nargs=None, help='name of category') 
-parser.add_argument("-il", nargs="?", help='name of IL for individual level categories', required=False) 
+parser = argparse.ArgumentParser(
+    "Dump speedrun.com leaderboard to JSON/CSV/SQLite")
+parser.add_argument('game', metavar='G', nargs=None, help='id of game')
+parser.add_argument('category', metavar='C',
+                    nargs=None, help='name of category')
+parser.add_argument(
+    "-il", nargs="?", help='name of IL for individual level categories', required=False)
 parser.add_argument('--json', action='store_true', help="output JSON")
 parser.add_argument('--csv', action='store_true', help="output CSV")
 parser.add_argument('--sqlite', action='store_true', help="output SQLite DB")
@@ -24,7 +28,9 @@ requests_cache.install_cache()
 api = srcomapi.SpeedrunCom()
 api.debug = 1
 
-@retry(wait=wait_exponential(multiplier=1, min=4, max=20))
+# @retry(wait=wait_exponential(multiplier=1, min=4, max=20))
+
+
 def get_game_leaderboards(game_id):
     game = api.get_game(game_id)
 
@@ -32,23 +38,34 @@ def get_game_leaderboards(game_id):
     for category in game.categories:
         if not category.name in lbs:
             lbs[category.name] = {}
-        if category.type == 'per-level':
+        if category.type == 'per-level' and args.il:
             for level in game.levels:
                 lbs[category.name][level.name] = dt.Leaderboard(api, data=api.get(
                     "leaderboards/{}/level/{}/{}?embed=variables".format(game.id, level.id, category.id)))
-        else:
+        elif category.type == 'per-game':
             lbs[category.name] = dt.Leaderboard(api, data=api.get(
                 "leaderboards/{}/category/{}?embed=variables".format(game.id, category.id)))
 
     return lbs
 
-@retry(wait=wait_exponential(multiplier=1, min=4, max=20))
+# @retry(wait=wait_exponential(multiplier=1, min=4, max=20))
+
+
 def append_run(r, runs):
     subcategory = ""
+   # if r.values:
+    # variable_id = next(iter(r.values.keys()))
+    # var = dt.Variable(api, api.get("variables/{}".format(variable_id)))
+    #subcategory = var.values["values"][r.values[variable_id]]["label"]
+
     if r.values:
-        variable_id = next(iter(r.values.keys()))
-        var = dt.Variable(api, api.get("variables/{}".format(variable_id)))
-        subcategory = var.values["values"][r.values[variable_id]]["label"]
+        subcategory = ", ".join(
+            list(map(lambda k: dt.Variable(api, api.get("variables/{}".format(k))).values["values"][r.values[k]]["label"], r.values.keys())))
+
+    videos = ""
+    if not r.videos is None:
+        videos = ", ".join(list(map(lambda v: v['uri'], r.videos['links']))) if r.videos.get(
+            "text") is None else r.videos.get("text")
 
     runs.append(
         {
@@ -60,21 +77,26 @@ def append_run(r, runs):
             "emulated": r.system['emulated'],
             "date": r.date,
             "comment": r.comment,
-            "videos": ", ".join(list(map(lambda v: v['uri'], r.videos['links'])) if not r.videos is None else [])
+            "videos": videos
         }
     )
 
+
 def get_runs_list(lb):
     runs = []
-    
+    i = 0
     for run in lb.runs:
+        print(i)
         append_run(run['run'], runs)
+        i = i + 1
 
     return runs
 
+
 lbs = get_game_leaderboards(args.game)
 
-runs = get_runs_list(lbs[args.category] if args.il is None else lbs[args.il][args.category])
+runs = get_runs_list(
+    lbs[args.category] if args.il is None else lbs[args.il][args.category])
 
 runs_json = json.dumps(runs)
 
@@ -97,13 +119,16 @@ if args.csv:
 if args.sqlite:
     connection = sqlite3.connect('out/srcom.sqlite')
     cursor = connection.cursor()
-    cursor.execute('Create TABLE if not exists {} (game text, category text, subcategory text, player text, time real, platform text, region text, emulated integer, date text, comment text, link text, cheated integer, [editor\'s note] text)'.format(args.game))
+    cursor.execute(
+        'Create TABLE if not exists {} (game text, category text, subcategory text, player text, time real, platform text, region text, emulated integer, date text, comment text, link text, cheated integer, [editor\'s note] text)'.format(args.game))
 
     columns = list(runs[0].keys())
     for row in runs:
-        keys = (args.game, args.category) + tuple(row[c] for c in columns) + (False, None)
+        keys = (args.game, args.category) + \
+            tuple(row[c] for c in columns) + (False, None)
         hash = hashlib.sha256(repr(keys).encode()).hexdigest()
-        cursor.execute('insert into {} values(?,?,?,?,?,?,?,?,?,?,?,?,?)'.format(args.game), keys)
+        cursor.execute(
+            'insert into {} values(?,?,?,?,?,?,?,?,?,?,?,?,?)'.format(args.game), keys)
 
     connection.commit()
     connection.close()
